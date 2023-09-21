5220c309e5c49fd9696e1d70d5e6ae979774c74e
dllo09 <dllo09@naver.com>
2023-09-21 22:56:13 +0900

    @SessionAttributes 어노테이션 추가

--- src/main/java/com/toy/cafesearch/controller/CafePageController.java
+++ src/main/java/com/toy/cafesearch/controller/CafePageController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
+@SessionAttributes({"loginMember"})
 @Controller
 @RequestMapping("/cafe")
 public class CafePageController {
--- src/main/java/com/toy/cafesearch/controller/ReviewPageController.java
+++ src/main/java/com/toy/cafesearch/controller/ReviewPageController.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @RequiredArgsConstructor
+@SessionAttributes({"loginMember"})
 @Controller
 @RequestMapping("/cafe")
 public class ReviewPageController {
