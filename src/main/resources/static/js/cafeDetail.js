2d2392d2bd9d072defcf5daf4ee5c5b837f71433
dllo09 <dllo09@naver.com>
2023-09-22 22:32:53 +0900

    카페 상세 페이지에 지도 나오게 함

--- src/main/resources/static/js/cafeDetail.js
+++ src/main/resources/static/js/cafeDetail.js
@@ -27,3 +27,10 @@ const reviewDelete = (reviewNo)=>{
         });
     }
 }
+
+var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+}
+
+var map = new naver.maps.Map('map', mapOptions);
