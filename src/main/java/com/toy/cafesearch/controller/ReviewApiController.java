e08ed629aa9cd0258de42540131e3202d6636f3c
dllo09 <dllo09@naver.com>
2023-10-12 22:35:42 +0900

    메소드 리턴 타입을 ResponseEntity로 변경

--- src/main/java/com/toy/cafesearch/controller/ReviewApiController.java
+++ src/main/java/com/toy/cafesearch/controller/ReviewApiController.java
@@ -7,6 +7,8 @@ import com.toy.cafesearch.dto.Member;
 import com.toy.cafesearch.dto.Review;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,17 +27,17 @@ public class ReviewApiController {
     private final CafeService cafeService;
 
     @GetMapping("/{reviewNo}")
-    public Review getReview(@PathVariable int reviewNo){
+    public ResponseEntity<Review> getReview(@PathVariable int reviewNo){
         Optional<Review> optionalReview = reviewService.findByReviewNo(reviewNo);
         if (optionalReview.isEmpty()){
-            return null;
+            return new ResponseEntity<Review>(HttpStatus.OK);
         }else {
-            return optionalReview.get();
+            return new ResponseEntity<Review>(optionalReview.get(), HttpStatus.OK);
         }
     }
 
     @PostMapping("/")
-    public int postReview(@RequestParam(value = "postElements[]") List<String> elements){
+    public ResponseEntity postReview(@RequestParam(value = "postElements[]") List<String> elements){
         String title = elements.get(0);
         String cafeName = elements.get(1);
         int star = Integer.parseInt(elements.get(2));
@@ -61,11 +63,11 @@ public class ReviewApiController {
 
         reviewService.saveReview(review, cafe);
 
-        return 0;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PutMapping("/{reviewNo}")
-    public void updateReview(@RequestBody List<String> elements, @PathVariable int reviewNo){
+    public ResponseEntity updateReview(@RequestBody List<String> elements, @PathVariable int reviewNo){
         String title = elements.get(0);
         String cafeName = elements.get(1);
         int star = Integer.parseInt(elements.get(2));
@@ -85,11 +87,15 @@ public class ReviewApiController {
                 .content(content).build();
 
         reviewService.updateReview(reviewNo, updateReview);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping("/{reviewNo}")
-    public void deleteReview(@PathVariable int reviewNo){
+    public ResponseEntity deleteReview(@PathVariable int reviewNo){
         reviewService.deleteReview(reviewNo);
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
